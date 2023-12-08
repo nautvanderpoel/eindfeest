06065e7d0236a9f98a6ef771eb41b7a102d79135
nautvanderpoel <135599215+nautvanderpoel@users.noreply.github.com>
2023-12-08 14:35:37 +0100

    ADDED CLI

--- src/solarcell/solarcell_view.py
+++ src/solarcell/solarcell_view.py
@@ -7,16 +7,16 @@ import matplotlib.pyplot as plt
 def cmd_group():
     pass
 
-@cmd_group.command(help= "gives list of all available ports for connected arduino device")
-def list():
-    """function that returns back the available ports to the arduino device from the model
+# @cmd_group.command(help= "gives list of all available ports for connected arduino device")
+# def list():
+#     """function that returns back the available ports to the arduino device from the model
 
-    Returns:
-        list: available ports
-    """
-    return print(find_ports())
+#     Returns:
+#         list: available ports
+#     """
+    # return print(find_ports())
 
-    
+
 @cmd_group.command()
 def plot():
     measurements = DiodeExperiment
