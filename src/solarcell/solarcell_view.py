e64a665a21577b4d6f0d4d048dcb811e1b507deb
nautvanderpoel <135599215+nautvanderpoel@users.noreply.github.com>
2023-12-12 10:42:38 +0100

    added FF, but it is not correct yet

--- src/solarcell/solarcell_view.py
+++ src/solarcell/solarcell_view.py
@@ -2,6 +2,7 @@ from solarcell.solarcell_model import DiodeExperiment
 import click
 import csv
 import matplotlib.pyplot as plt
+import numpy as np 
 
 @click.group()
 def cmd_group():
@@ -14,14 +15,15 @@ def cmd_group():
 # @cmd_group.command()
 def plot():
     measurements = DiodeExperiment(port = "ASRL5::INSTR")
-    U_solarcell_list_mean, I_solarcell_list_mean, error_I_solarcell_list, error_U_solarcell_list, R_var_list_mean, error_R_var_list, P_solarcell_list_mean, error_P_solarcell_list = measurements.scan(start = 1.56, stop = 1.71, number = 5)
+    U_solarcell_list_mean, I_solarcell_list_mean, error_I_solarcell_list, error_U_solarcell_list, R_var_list_mean, error_R_var_list, P_solarcell_list_mean, error_P_solarcell_list,fill_factor = measurements.scan(start = 1.56, stop = 1.71, number = 5)
     plt.errorbar(U_solarcell_list_mean,I_solarcell_list_mean, xerr =error_U_solarcell_list, yerr = error_I_solarcell_list, fmt = '.' )
     plt.title("Solarcell graph voltage (V) against current (I)")
     plt.xlabel("Voltage (V)")
     plt.ylabel("Current (I)")
     plt.show()
-    plt.errorbar(P_solarcell_list_mean,R_var_list_mean, xerr =error_P_solarcell_list, yerr = error_R_var_list, fmt = '.' )
-    plt.xlabel("Power (W) solarcell")
-    plt.ylabel("Variable resistancte (Ohm)")
+    plt.errorbar(R_var_list_mean,P_solarcell_list_mean, xerr = error_R_var_list,yerr =error_P_solarcell_list, fmt = '.' )
+    plt.xlabel("Variable resistancte (Ohm)")
+    plt.ylabel("Power solarcell (W)")
     plt.show()
+    print(f"{fill_factor=}")
 plot()
